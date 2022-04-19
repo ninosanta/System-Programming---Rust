057b8a0a8430f9e02cf0ae61671c06347d198262
ninosanta <38584348+ninosanta@users.noreply.github.com>
2022-04-19 22:28:59 +0200

    final

--- Labs/Lab02/clock/src/lib.rs
+++ Labs/Lab02/clock/src/lib.rs
@@ -1,15 +1,36 @@
-pub struct Clock;
+use std::fmt;
+use std::fmt::Formatter;
+
+const HOUR: i32 = 60;
+const DAY: i32 = 24 * HOUR;
+
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        let tot_minutes = ((hours * HOUR + minutes) % DAY + DAY) % DAY;
+        Clock {
+            hours: tot_minutes / HOUR,
+            minutes: tot_minutes % HOUR,
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        Clock::new(self.hours, self.minutes + minutes)
+    }
+
+    pub fn to_string(&self) -> String {
+        format!("{:02}:{:02}", &self.hours, &self.minutes)
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
